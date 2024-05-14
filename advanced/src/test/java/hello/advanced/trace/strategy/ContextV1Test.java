c26b9b59fa724b6e7e106456c364f73ab72fd61c
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-05-15 01:22:59 +0900

    전략 패턴 예제2,3

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -47,5 +48,25 @@ public class ContextV1Test {
 		StrategyLogic2 strategyLogic2 = new StrategyLogic2();
 		ContextV1 context2 = new ContextV1(strategyLogic2);
 		context2.execute();
+	}
+
+
+	@Test
+	void strategyV2() {
+		Strategy strategyLogic1 = new Strategy() {
+
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		ContextV1 contextV1 = new ContextV1(strategyLogic1);
+		contextV1.execute();
+	}
+
+	@Test
+	void strategyV3() {
+		ContextV1 contextV1 = new ContextV1(()  -> log.info("비즈니스 로직1 실행"));
+		contextV1.execute();
 	}
 }
