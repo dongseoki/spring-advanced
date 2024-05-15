b0063c735800f96208a08c05e2adce664e0ae81e
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-05-15 11:10:27 +0900

    인터페이스 기반 프록시 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,9 +2,13 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -12,12 +16,17 @@ import org.springframework.context.annotation.Import;
  *  `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
  *  따라서 스캔 대상의 범위를 "hello.proxy.app.v3" 과 같은 방식으로 변경해야한다.
  */
-@Import({AppV2Config.class})
+// @Import({AppV1Config.class, AppV2Config.class})
+@Import({InterfaceProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
